2a38fc0d950c2ca1f9650eacd8f3e6b936880e08
keir stannard <keir@Keirs_PC.(none)>
2018-02-13 18:24:53 +0000

    created first page with button click working

--- app/src/main/java/com/example/stannard/therapybox/MainActivity.java
+++ app/src/main/java/com/example/stannard/therapybox/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.stannard.therapybox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton loginButton;
+    private EditText usernameEditText, passwordEditText;
+    private TextView signupTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (ImageButton) findViewById(R.id.loginButton);
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        signupTextView = (TextView) findViewById(R.id.signupLinktextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.print("Click");
+            }
+        });
+
     }
 }
